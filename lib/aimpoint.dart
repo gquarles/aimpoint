49b9d77d2b8f1bdab9a4565f3bb57b407703b6e4
Griffin Quarles <gquarles@protonmail.com>
2022-07-21 18:24:49 -0400

    update

---
+++ lib/aimpoint.dart
@@ -0,0 +1,536 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AimpointLocation {
+  final double theta;
+  final double magnitude;
+  final bool negativeY;
+
+  AimpointLocation(
+      {required this.theta, required this.magnitude, required this.negativeY});
+
+  Offset offset(double radius) {
+    var scalar = magnitude * radius;
+    var directionx = scalar * -sin(theta);
+    var directiony = scalar * -cos(theta);
+    if (negativeY) directiony = -directiony;
+    if (negativeY) directionx = -directionx;
+
+    return Offset(directionx, directiony);
+  }
+
+  static AimpointLocation fromCartesian(
+      BoxConstraints constraints, double dx, double dy, double radius) {
+    //Get x,y for center of target
+    var xorigin = constraints.maxWidth / 2;
+    var yorigin = constraints.maxHeight / 2;
+
+    //Direction of vector from center of target
+    var directionx = dx - xorigin;
+    var directiony = dy - yorigin;
+
+    //Magnitude of vector
+    var magnitude = sqrt(directionx * directionx + directiony * directiony);
+
+    //Get what % of the radius is the magnitude
+    var magnitudeAdjusted = magnitude / radius;
+    //Grab angle in radians
+    var theta = atan(directionx / directiony);
+
+    //If bottom half we flip sign of x and y
+    var negativeY = (directiony < 0) ? false : true;
+
+    return AimpointLocation(
+        theta: theta, magnitude: magnitudeAdjusted, negativeY: negativeY);
+  }
+
+  static Future<AimpointLocation?> load(String name) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final theta = prefs.getDouble('$name/theta');
+    final magnitude = prefs.getDouble('$name/magnitude');
+    final negativeY = prefs.getBool('$name/negativeY');
+
+    if (theta == null || magnitude == null || negativeY == null) return null;
+    return AimpointLocation(
+        magnitude: magnitude, negativeY: negativeY, theta: theta);
+  }
+
+  static Future<void> save(String name, AimpointLocation aimpoint) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('$name/theta', aimpoint.theta);
+    await prefs.setDouble('$name/magnitude', aimpoint.magnitude);
+    await prefs.setBool('$name/negativeY', aimpoint.negativeY);
+    return;
+  }  
+}
+
+class Target {
+  late double radius;
+  late double ringSize;
+  late double seperatorSize;
+  final BoxConstraints constraints;
+
+  Target({required this.constraints}) {
+    radius = (constraints.constrainWidth(constraints.maxHeight) * .95) / 2;
+    ringSize = radius * 0.104;
+    seperatorSize = radius * 0.0125;
+  }
+
+  int findSelectedRing(AimpointLocation aimpoint) {
+    if (aimpoint.magnitude * radius < radius) {
+      for (int ring = 0; ring < 13; ring++) {
+        if (!(aimpoint.magnitude * radius < radius - (ringSize * ring) )) {
+          return ring;
+        }
+      }  
+    }
+
+    return -1;
+  }
+
+  static Widget drawRing(
+      Color color, double size, double seperatorSize, double ringSize,
+      {Color seperatorColor = Colors.black,
+      Offset offset = Offset.zero,
+      bool drawName = false, bool selected = false}) {
+    
+    if (selected) {
+      if (color == Colors.blue) color = Colors.blue[800]!;
+      if (color == Colors.red) color = Colors.red[800]!;
+      if (color == Colors.black) color = Colors.grey[800]!;
+      if (color == Colors.white) color = Colors.white70;
+      if (color == Colors.yellow) color = Colors.yellow[800]!;
+      //size = size + 2;
+      seperatorSize = seperatorSize * 2;
+    }
+    return Stack(
+      children: [
+        Container(
+          width: 0,
+          height: 0,
+          child: CustomPaint(
+            painter: CirclePainter(
+              color: seperatorColor,
+              offset: offset,
+              radius: size + seperatorSize,
+            ),
+          ),
+        ),
+        Container(
+          width: 0,
+          height: 0,
+          child: CustomPaint(
+            painter: CirclePainter(
+              color: color,
+              offset: offset,
+              radius: size,
+            ),
+          ),
+        ),
+        Container(
+          width: 0,
+          height: 0,
+          child: CustomPaint(
+            painter: CirclePainter(
+              color: seperatorColor,
+              offset: offset,
+              radius: size - ringSize,
+            ),
+          ),
+        ),
+        Container(
+          width: 0,
+          height: 0,
+          child: CustomPaint(
+            painter: CirclePainter(
+              color: Colors.white,
+              offset: offset,
+              radius: size - seperatorSize - ringSize,
+              drawName: drawName,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  static List<Widget> buildTarget(Target target, {int selectedRing = -1}) {
+    return [
+      Center(
+        child: drawRing(
+          Colors.white,
+          target.radius,
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 1
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.white,
+          target.radius - target.ringSize,
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 2
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.black,
+          target.radius - (target.ringSize * 2),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          seperatorColor: Colors.black,
+          selected: selectedRing == 3
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.black,
+          target.radius - (target.ringSize * 3),
+          target.seperatorSize - 1,
+          target.ringSize - target.seperatorSize,
+          seperatorColor: Colors.white,
+          selected: selectedRing == 4
+          
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.blue,
+          target.radius - (target.ringSize * 4),
+          target.seperatorSize + 1,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 5
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.blue,
+          target.radius - (target.ringSize * 5),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 6
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.red,
+          target.radius - (target.ringSize * 6),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 7
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.red,
+          target.radius - (target.ringSize * 7),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 8
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.yellow,
+          target.radius - (target.ringSize * 8),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 9
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.yellow,
+          target.radius - (target.ringSize * 9),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 10
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.yellow,
+          target.radius - (target.ringSize * 10),
+          target.seperatorSize,
+          target.ringSize - target.seperatorSize,
+          selected: selectedRing == 11
+        ),
+      ),
+      Center(
+        child: drawRing(
+          Colors.yellow,
+          target.radius - (target.ringSize * 11),
+          target.seperatorSize + 1,
+          target.ringSize - target.seperatorSize,
+          seperatorColor: Colors.yellow,
+          selected: selectedRing == 12
+        ),
+      ),
+    ];
+  }
+}
+
+class TargetWidgetSmall {
+  static ConstrainedBox buildTarget(String name, {double width = 50, bool smallAimpoint = true}) {
+    return ConstrainedBox(constraints: BoxConstraints.loose(Size.fromWidth(width)), child: TargetWidget(addingArrows: false, allowEdit: false, name: name, targetController: TargetController(), smallAimpoint: smallAimpoint,));
+  }
+}
+
+class TargetController {
+  late void Function() clearArrows;
+  late void Function() shareTarget;
+  late void Function() fitAimpoint;
+}
+
+class TargetWidget extends StatefulWidget {
+  final TargetController targetController;
+  final String name;
+  final bool allowEdit;
+  final bool addingArrows;
+  final Color backgroundColor;
+  final bool smallAimpoint;
+
+  const TargetWidget(
+      {Key? key, required this.name,
+      required this.allowEdit,
+      required this.addingArrows,
+      required this.targetController, this.backgroundColor=Colors.transparent, this.smallAimpoint = false}) : super(key: key);
+
+  @override
+  // ignore: no_logic_in_create_state
+  TargetWidgetState createState() => TargetWidgetState(targetController);
+}
+
+class TargetWidgetState extends State<TargetWidget> {
+  var aimpoint = AimpointLocation(magnitude: 0, negativeY: false, theta: 0);
+  List<AimpointLocation> arrows = [];
+  final GlobalKey _globalKey = GlobalKey();
+  int selectedRing = -1;
+
+  TargetWidgetState(TargetController targetController) {
+    targetController.clearArrows = clearArrows;
+    targetController.fitAimpoint = fitAimpoint;
+  }
+
+  void clearArrows() {
+    setState(() {
+      arrows.clear();
+    });
+    saveArrows();
+  }
+
+  void fitAimpoint() {}
+
+  @override
+  void initState() {
+    AimpointLocation.load(widget.name).then((value) {
+      if (value != null) {
+        loadArrows();
+        setState(() {
+          aimpoint = value;
+        });
+      }
+    });
+    super.initState();
+  }
+
+  Future<void> loadArrows() async {
+    String path = '${widget.name}/arrows';
+    var prefs = await SharedPreferences.getInstance();
+
+    var count = prefs.getInt('$path/count');
+    count ??= 0;
+    List<AimpointLocation> tempArrows = [];
+    for (int i = 0; i < count; i++) {
+      var arrow = await AimpointLocation.load('$path/$i');
+      if (arrow != null) tempArrows.add(arrow);
+    }
+    setState(() {
+      arrows = tempArrows;
+    });
+  }
+
+  Future<void> saveArrows() async {
+    String path = '${widget.name}/arrows';
+
+    var prefs = await SharedPreferences.getInstance();
+
+    for (int i = 0; i < arrows.length; i++) {
+      await AimpointLocation.save('$path/$i', arrows[i]);
+    }
+    await prefs.setInt('$path/count', arrows.length);
+  }
+
+  void onTargetTap(
+      Offset offset, BoxConstraints display, Target target, bool save) {
+    if (widget.addingArrows && save == false) return;
+
+    var newAimpoint = AimpointLocation.fromCartesian(display, offset.dx,offset.dy, target.radius);
+    
+    if (widget.addingArrows) {
+      setState(() {
+        arrows.add(newAimpoint);
+      });
+      saveArrows();
+    } else {
+      if (save) AimpointLocation.save(widget.name, newAimpoint);
+      setState(() {
+        aimpoint = newAimpoint;
+        selectedRing = target.findSelectedRing(newAimpoint);
+      });
+    }
+  }
+
+  Widget wrapGesture(
+      {required Widget child,
+      required BoxConstraints display,
+      required Target target}) {
+    return widget.allowEdit
+        ? GestureDetector(
+            onTapUp: (details) => onTargetTap(Offset(details.localPosition.dx, details.localPosition.dy), display, target, true),
+            onHorizontalDragUpdate: (details) => onTargetTap(Offset(details.localPosition.dx, details.localPosition.dy), display, target, false),
+            onHorizontalDragStart: (details) => onTargetTap(Offset(details.localPosition.dx, details.localPosition.dy), display, target, false),
+            onHorizontalDragEnd: (details) => AimpointLocation.save(widget.name, aimpoint),
+            child: child,
+          )
+        : child;
+  }
+
+  Widget buildAimpoint(
+      {required Target target,
+      required Color color,
+      required AimpointLocation aimpoint}) {
+    var size = 10.0;
+    var sepSize = 2.0;
+    var ringSize = 5.0;
+
+    if (widget.smallAimpoint) {
+      size = 7;
+      sepSize = 1;
+      ringSize = 4;
+    }
+    return Center(
+      child: Target.drawRing(
+        color,
+        size,
+        sepSize,
+        ringSize,
+        offset: aimpoint.offset(target.radius),
+      ),
+    );
+  }
+
+  List<Widget> buildArrows(Target target) {
+    List<Widget> arrowWidgets = [];
+
+    for (var arrow in arrows) {
+      arrowWidgets.add(
+          buildAimpoint(target: target, color: Colors.red, aimpoint: arrow));
+    }
+
+    return arrowWidgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, display) {
+        var target = Target(constraints: display);
+        return wrapGesture(
+          display: display,
+          target: target,
+          child: RepaintBoundary(
+            key: _globalKey,
+            child: Stack(
+              children: [
+                // Background color
+                SizedBox.expand(
+                  child: Container(
+                    color: widget.backgroundColor,
+                  ),
+                ),
+
+                // Target rings
+                ...Target.buildTarget(target, selectedRing: selectedRing),
+
+                // Arrows
+                ...buildArrows(target),
+
+                // Aimpoint
+                buildAimpoint(
+                  target: target,
+                  color: Colors.greenAccent,
+                  aimpoint: aimpoint,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class CirclePainter extends CustomPainter {
+  final double radius;
+  final Color color;
+  final Offset offset;
+  final bool drawName;
+
+  CirclePainter(
+      {required this.radius,
+      required this.color,
+      required this.offset,
+      this.drawName = false});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (drawName) {
+      TextSpan span = const TextSpan(
+          style: TextStyle(color: Colors.black), text: 'Fitted Aimpoint');
+      TextPainter tp = TextPainter(
+          text: span,
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.ltr);
+      tp.layout();
+
+      canvas.drawRRect(
+          RRect.fromRectAndRadius(
+            Offset(offset.dx - 48, offset.dy + 13) & const Size(100, 20),
+            const Radius.circular(5),
+          ),
+          Paint()..color = color);
+      tp.paint(
+        canvas,
+        Offset(offset.dx - 46, offset.dy + 13),
+      );
+
+      const p1 = Offset(0, 0);
+
+      canvas.drawLine(
+        offset,
+        p1,
+        Paint()
+          ..color = Colors.black
+          ..strokeWidth = 4,
+      );
+    }
+
+    canvas.drawCircle(
+      offset,
+      radius,
+      Paint()
+        ..color = color
+        ..style = PaintingStyle.fill,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: sized_box_for_whitespace
-
+import 'package:aimpoint/aimpoint.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MaterialApp(home: AimpointPage(), debugShowCheckedModeBanner: false,));
+  runApp(const MaterialApp(
+    home: AimpointPage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class AimpointPage extends StatefulWidget {
@@ -17,78 +19,7 @@ class AimpointPage extends StatefulWidget {
 }
 
 class AimpointPageState extends State<AimpointPage> {
-  Offset _currentOffset = Offset.zero;
-  double yPercentFromCenter = 0;
-  double xPercentFromCenter = 0;
-  List<Widget> arrowCircles = [];
-
-  loadOffset() async {
-    //Get offset from shared preferences
-    final prefs = await SharedPreferences.getInstance();
-    final offsetx = prefs.getDouble('offsetx');
-    final offsety = prefs.getDouble('offsety');
-    setState(() {
-      yPercentFromCenter = offsety ?? 0;
-      xPercentFromCenter = offsetx ?? 0;
-    });
-  }
-
-  saveOffset(double x, double y) async {
-    //Save offset to shared preferences
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setDouble('offsetx', x);
-    prefs.setDouble('offsety', y);
-  }
-
-  @override
-  void initState() {
-    loadOffset();
-    super.initState();
-  }
-
-  Widget drawRing(
-      Color color, double size, double seperatorSize, double ringSize,
-      {Color seperatorColor = Colors.black, Offset offset = Offset.zero}) {
-    return Stack(
-      children: [
-        Container(
-          width: 0,
-          height: 0,
-          child: CustomPaint(
-            painter: CirclePainter(
-                color: seperatorColor,
-                offset: offset,
-                radius: size + seperatorSize,),
-          ),
-        ),
-        Container(
-          width: 0,
-          height: 0,
-          child: CustomPaint(
-            painter: CirclePainter(color: color, offset: offset, radius: size,),
-          ),
-        ),
-        Container(
-          width: 0,
-          height: 0,
-          child: CustomPaint(
-            painter: CirclePainter(
-                color: seperatorColor, offset: offset, radius: size - ringSize,),
-          ),
-        ),
-        Container(
-          width: 0,
-          height: 0,
-          child: CustomPaint(
-            painter: CirclePainter(
-                color: Colors.white,
-                offset: offset,
-                radius: size - seperatorSize - ringSize,),
-          ),
-        ),
-      ],
-    );
-  }
+  TargetController targetController = TargetController();
 
   @override
   Widget build(BuildContext context) {
@@ -100,109 +31,22 @@ class AimpointPageState extends State<AimpointPage> {
         elevation: 0,
         iconTheme: const IconThemeData(color: Colors.black),
         title: const Text('Aimpoint', style: TextStyle(color: Colors.black)),
-        
       ),
-      body: Center(
-        child: ConstrainedBox(constraints: BoxConstraints.loose(const Size.fromWidth(200)),
-        child: LayoutBuilder(
-          builder: (context, display) {
-            var width = display.constrainWidth(display.maxHeight) * .95;
-            var height = display.constrainHeight(display.maxWidth) * .95;
-            var r = width / 2;
-            //var ry = (display.constrainHeight(display.maxWidth) * .95) / 2;
-            var ringSize = r * 0.104;
-            var sepSize = r * 0.0125;
-            
-            var yFromCenter = yPercentFromCenter * height;
-            var xFromCenter = xPercentFromCenter * width;
-            _currentOffset = Offset(xFromCenter, yFromCenter);
-
-            return GestureDetector(
-              onTapUp: (details) {
-                var xorigin = display.maxWidth / 2;
-                var yorigin = display.maxHeight / 2;
-
-                var directionx = details.localPosition.dx - xorigin;
-                var directiony = details.localPosition.dy - yorigin;
-                //directiony = -directiony;
-                //var magnitude = sqrt(directionx * directionx + directiony * directiony);
-
-                //print('mag: $magnitude');
-                //print('x_dir: $directionx');
-                //print('y_dir: $directiony');
-                //directionx = -directionx;
-                //directiony = -directiony;
-                //var yPercent = (details.localPosition.dy / height);
-                //var xPercent = (details.localPosition.dx / width);
-                var yPercent = (directiony / height);
-                var xPercent = (directionx / width);
-                
-
-                yPercentFromCenter = yPercent; //- 0.50;
-                xPercentFromCenter = xPercent; //- 0.50;
-                var yFromCenter = yPercentFromCenter * height;
-                var xFromCenter = xPercentFromCenter * width;
-                
-                saveOffset(xPercentFromCenter, yPercentFromCenter);
-                setState(() {
-                  _currentOffset = Offset(xFromCenter, yFromCenter);
-                });
-              },
-              child: Stack(
-                children: [
-                  SizedBox.expand(
-                    child: Container(
-                      color: Colors.yellow[50],
-                    ),
-                  ),
-                  Center(child: drawRing(Colors.white, r, sepSize, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.white, r - ringSize, sepSize, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.black, r - (ringSize * 2), sepSize, ringSize - sepSize, seperatorColor: Colors.black)),
-                  Center(child: drawRing(Colors.black, r - (ringSize * 3), sepSize - 1, ringSize - sepSize, seperatorColor: Colors.white)),
-                  Center(child: drawRing(Colors.blue, r - (ringSize * 4), sepSize + 1, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.blue, r - (ringSize * 5), sepSize, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.red, r - (ringSize * 6), sepSize, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.red, r - (ringSize * 7), sepSize, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.yellow, r - (ringSize * 8), sepSize, ringSize - sepSize, )),
-                  Center(child: drawRing(Colors.yellow, r - (ringSize * 9), sepSize, ringSize - sepSize)),
-                  Center(child: drawRing(Colors.yellow, r - (ringSize * 10), sepSize, ringSize - sepSize,)),
-                  Center(child: drawRing(Colors.yellow, r - (ringSize * 11), sepSize + 1, ringSize - sepSize, seperatorColor: Colors.yellow)),
-                  Center(child: drawRing(Colors.greenAccent, 10, 2, 5, offset: _currentOffset)),
-                  ///...arrowCircles,
-                  //Center(child: drawRing(Colors.red, 10, 2, 5, offset: Offset(r, ry))),
-                  //drawRing(Colors.redAccent, 10, 2, 5, offset: _currentOffset)
-                  //ClipOval(
-                  //  child: Image.network(
-                  //      'https://cdn.shopify.com/s/files/1/1100/1512/products/80cm_Paper_Face_44bd121d-6181-4a9b-9fb8-ea55bb75c6d8_1800x.png'),
-                  //),
-                ],
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: TargetWidget(
+                allowEdit: true,
+                name: 'aimpoint',
+                addingArrows: false,
+                targetController: targetController,
+                backgroundColor: Colors.yellow[50]!,
               ),
-            );
-          }
-        ),),
-      )
+            ),
+          ),
+        ],
+      ),
     );
   }
-}
-
-class CirclePainter extends CustomPainter {
-  final double radius;
-  final Color color;
-  final Offset offset;
-
-  CirclePainter(
-      {required this.radius, required this.color, required this.offset});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(
-        offset,
-        radius,
-        Paint()
-          ..color = color
-          ..style = PaintingStyle.fill);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
